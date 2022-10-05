535d6a751e836f1cffc7abb8a8de30db3413c0f9
codej625 <codej625@gmail.com>
2022-10-06 00:13:01 +0900

    20221006 풀이

--- study/src/study/Test.java
+++ study/src/study/Test.java
@@ -4,70 +4,25 @@ public class Test {
 	
 	public static void main(String[] args) {
 		
-		/**
-		 * 1. 한가지의 변수와 세 가지의 배열을 사용하여 아래와 같이 콘솔 창에 출력되게 만드시오.
-		 * =================
-		   아이유 조보아 사나 
-		   나연 채령 예지 
-		   이유비 장원영 해서웨이 
-		   =================
-		 * 
-		 * 힌트1: 부모 for 문의 변수를 잘 활용해야 할지도?
-		 */
-		int arrayLength = 3;
+		// 전역변수 선언
+		int num = 5;
 		
-		String[] aClass = { "아이유", "조보아", "사나" };
-		String[] bClass = { "나연", "채령", "예지" };
-		String[] cClass = { "이유비", "장원영", "해서웨이" };
-
-		for (int i = 0; i < arrayLength; i++) {
-
-			if (i == 0) {
-				System.out.println("=================");
+		
+		for (int i = 0; i <= num; i++) {
+			
+			// 부모 for문의 변수 사용
+			/**
+			 * j는 i보다 작을때까지만 반복하기 때문에 처음에는 한번만 반복됨
+			 * 두번째는 i는 1이 되어서 두번 반복
+			 * 세번째는 i는 2가 되어서 세번 반복
+			 * 이런식으로 j는 i만큼 반복 
+			 */
+			for (int j = 0; j < i; j++) {
+				// \n이 붙어있는 println이 아니므로 개행 되지않고 같은 line에 출력
+				System.out.printf("*");
 			}
-
-			for (int j = 0; j < arrayLength; j++) {
-				
-				switch (i) {
-				case  0: System.out.print(aClass[j] + " "); break;
-				case  1: System.out.print(bClass[j] + " "); break;
-				default: System.out.print(cClass[j] + " "); break;
-				}
-//				if (i == 0) {
-//					System.out.print(aClass[j] + " ");
-//				} else if (i == 1) {
-//					System.out.print(bClass[j] + " ");
-//				} else {
-//					System.out.print(cClass[j] + " ");
-//				}
-			}
-			System.out.println();
-
-			if (i == arrayLength - 1) {
-				System.out.println("=================");
-			}
+			System.out.println("");
 		}
-		
-		/**
-		 * 2. 아래와 같이 콘솔 창에 출력되게 만드시오.
-		 * 
-		   ****
-    	   ****
-		   ****
-		   ****
-		 * 
-		 * 힌트2: 첫 번째 for 문은 반복의 역할을, 두 번째 for 문은 모양을 만드는 역할을 합니다.
-		 */
-		
-		int forLength = 4;
-		
-		for (int i = 0; i < forLength; i++ ) {
-			for (int j = 0; j < forLength; j++) {
-				System.out.print("*");
-			}
-			System.out.println();
-		}
-
 		
 	}
 
